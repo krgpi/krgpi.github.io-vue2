10b25fdfa72015f43bd36b136bce6c9a96ac5004
krgpi <ynoaki@me.com>
2021-08-13 11:39:45 +0900

    適宜boxに入れて見やすくする

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,13 @@
         <div class="item">
           <img src="img/karaage_free.jpg" alt="icon" width="250px" />
         </div>
-        <div class="item" style="max-width: 500px">
+        <div class="item category-box" style="max-width: 500px">
           <h1><b>カラアゲ / iKaraage</b></h1>
           <h3>Cinephotographer, Engineer</h3>
           <p>
             根っからのプロダクト志向を持ち、10年近く様々なジャンルで制作活動を続ける。フォトグラファーでありエンジニア。最近ではプロダクトマネージャーの仕事にも力を入れる。
+          </p>
+          <p>
             キーワード：iOS, Nuxt.js, Go, AWS, GCP,
             プロダクトマネージャー, α７ｓⅢ, 星景写真, 戦略と狂気, 破壊と創造
           </p>
