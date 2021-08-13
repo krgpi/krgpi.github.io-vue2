464850c6737f0163a9c4da7ba46001dd388663b9
krgpi <ynoaki@me.com>
2021-08-13 12:20:18 +0900

    パスのミスを修正

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,9 +4,9 @@
     <div id="header">
       <h1><a href="/">Portfolio</a></h1>
       <a href="/">Home</a>
-      <a href="gallery">Gallery</a>
-      <a href="works">Works</a>
-      <a href="career">Career</a>
+      <a href="/gallery">Gallery</a>
+      <a href="/works">Works</a>
+      <a href="/career">Career</a>
     </div>
   </div>
 </template>
