25b130d738de97a517644a259085dd6e75ffc8b3
krgpi <ynoaki@me.com>
2021-08-13 12:20:09 +0900

    Create krgpi.vue

---
+++ pages/krgpi.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="main_body">
+    <div class="body_container">
+      <p>
+        このページは移動しました。ブックマークをされてる方は、以下のURLへの変更をお願いいたします。
+      </p>
+      <a href="https://krgpi.github.io">https://krgpi.github.io</a>
+    </div>
+  </div>
+</template>
