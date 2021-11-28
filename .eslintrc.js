da7b1c78f03d8665cf98ee169de1057c9e348148
krgpi <ynoaki@me.com>
2021-11-28 23:31:17 +0900

    ライブラリ更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        htmlWhitespaceSensitivity: 'ignore'
+      }
+    ]
+  }
 }
