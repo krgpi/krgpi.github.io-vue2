9af8f9ee443c6bece6a2ca8ffa496ea41031bc8d
krgpi <17940589+krgpi@users.noreply.github.com>
2023-02-17 23:10:24 +0900

    titleを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'iKaraage - Portfolio',
+    title: 'Karaage - Software Engineer',
     htmlAttrs: {
       lang: 'ja'
     },
