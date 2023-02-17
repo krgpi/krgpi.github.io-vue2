3c9e2c458830b96297767fbe74fa98b3efcfc3e4
krgpi <17940589+krgpi@users.noreply.github.com>
2023-02-17 23:03:16 +0900

    GA設置

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,9 @@ export default {
       }
     ]
   },
+  googleAnalytics: {
+    id: 'G-EFW2B48B8G'
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.css'],
@@ -43,7 +46,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
