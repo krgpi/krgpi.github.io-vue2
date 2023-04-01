c2a974a0a8e8e4d47f6db5c865a1eac268c1475a
karaage (krgpi) <karaage@pixiv.co.jp>
2023-04-01 11:00:38 +0900

    2023/05の転職を反映

--- pages/career.vue
+++ pages/career.vue
@@ -5,14 +5,6 @@
 
       <p>より詳細な職務経歴やお仕事のオファーについては、<a href="https://github.com/krgpi">GitHub</a>に記載の連絡先よりご連絡ください。</p>
       <h2>現職</h2>
-      <div class="category-box">
-        <h2>2019.6-</h2>
-        <p>
-          <b>ピクシブ株式会社</b>
-          (フルタイムで在籍中)
-        </p>
-        <p>iOSアプリの開発等</p>
-      </div>
 
       <div class="category-box">
         <h2>2018.3-</h2>
@@ -23,18 +15,36 @@
         </p>
       </div>
 
+      <div class="category-box">
+        <h2>2023.5-</h2>
+        <p><b>株式会社CoeFont</b></p>
+        <mark class="tag">フルタイム</mark>
+        <p />
+      </div>
+
       <hr>
       <h2>過去</h2>
+
       <div class="category-box">
         <h2>2022.1-2022.12</h2>
         <p><b>株式会社WindHole</b></p>
         <p>最高技術責任者CTO・プロダクトオーナー・プロダクトマネージャー・Flutterアプリの開発・サービス設計・UIデザイン等</p>
       </div>
+
+      <div class="category-box">
+        <h2>2019.6-2023.4</h2>
+        <p>
+          <b>ピクシブ株式会社</b>
+        </p><mark class="tag">フルタイム</mark>
+        <p>新規事業部R&DでのiOSアプリの開発等</p>
+      </div>
+
       <div class="category-box">
         <h2>2017.9-2019.8</h2>
         <p><b>産業技術総合研究所（AIST）</b></p>
         <p>２号契約職員(テクニカルスタッフ)として研究活動補助等</p>
       </div>
+
       <hr>
       <h2>学歴</h2>
       <div class="category-box">
