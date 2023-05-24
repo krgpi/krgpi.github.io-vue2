0d38276d4ad78c0fa240dbb11d33810c6ce997d6
Karaage <karaage01@outlook.jp>
2023-05-24 11:44:18 +0900

    cloudstickiesの配布

--- pages/releases.vue
+++ pages/releases.vue
@@ -8,13 +8,13 @@
             Cloud Stickies
           </h2>
           <p>iCloudで同期できる付箋アプリです。</p>
+          <p><b><a href="/releases/cloudstickies/CloudStickiesInstaller.dmg">ダウンロード / Download</a></b></p>
           <h4>特徴</h4>
           <ul>
             <li>付箋を整理するウィンドウがあり、これはメニューバーから表示することもできます</li>
             <li>ピン留め機能：ピン留めした付箋は他のアプリ使用中も常に最前面に表示されます</li>
             <li>付箋の色を設定でき、これは端末間で同期されます</li>
           </ul>
-          <p><i>準備中... 2023年3月リリース目標</i></p>
           <hr>
           <p>You can sync sticky note via iCloud.</p>
           <h4>Features</h4>
@@ -23,7 +23,6 @@
             <li>Note can be pinned. Pinned note is always display front even if you would select other app window.</li>
             <li>You can set color of note, and it also be synchronized via iCloud.</li>
           </ul>
-          <p><i>Coming soon... maybe i can release it by March 2023.</i></p>
         </div>
         <h1>iOS App</h1>
         <div class="category-box">
@@ -31,7 +30,7 @@
             Cloud Stickies iOS
           </h2>
           <p>You can sync sticky note via iCloud.</p>
-          <p><i>Coming soon... maybe i can release it by March 2023.</i></p>
+          <p><i>Coming soon... maybe i can release it by June 2023.</i></p>
         </div>
       </div>
     </div>
